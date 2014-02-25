ba8dab03baa07afca4f5d67e420a22a073a202e2
Daniel Gottschalck <znowm4n@gmail.com>
2014-02-25 10:50:57 +0100

    updated

--- FacebookGroupJSON/FacebookGroupJSON/Feed.cs
+++ FacebookGroupJSON/FacebookGroupJSON/Feed.cs
@@ -8,9 +8,25 @@ namespace FacebookGroupJSON
 {
     class Feed
     {
+        #region Private Fields
+
+        private string _title;
+        private string _link;
+        private string _self;
+        private DateTime _updated;
+        private string _icon;
+        private List<FeedEntry> _feedEntries;
+
+        #endregion
+
         public Feed(string title, string link, string self, DateTime updated, string icon, List<FeedEntry> feedEntries)
         {
-            
+            _title = title;
+            _link = link;
+            _self = self;
+            _updated = updated;
+            _icon = icon;
+            _feedEntries = feedEntries;
         }
     }
 }
--- FacebookGroupJSON/FacebookGroupJSON/FeedEntry.cs
+++ FacebookGroupJSON/FacebookGroupJSON/FeedEntry.cs
@@ -7,9 +7,53 @@ namespace FacebookGroupJSON
 {
     class FeedEntry
     {
-        public FeedEntry()
+        #region Private Fields
+
+        private string _title;
+        private string _id;
+        private string _alternate;
+        private string[] _categories;
+        private DateTime _published;
+        private DateTime _updated;
+        private string _authorName;
+        private string _verb;
+        private string _target;
+        private string _objects;
+        private string _comments;
+        private string _likes;
+        private string _content;
+
+        #endregion Fields
+
+        #region Constructor
+
+        public FeedEntry(string title, string id, string alternate, string[] categories, DateTime published, DateTime updated, string authorName, string verb, string target, string objects, string comments, string likes, string content)
         {
-            
+            _title = title;
+            _id = id;
+            _alternate = alternate;
+            _categories = categories;
+            _published = published;
+            _updated = updated;
+            _authorName = authorName;
+            _verb = verb;
+            _target = target;
+            _objects = objects;
+            _comments = comments;
+            _likes = likes;
+            _content = content;
         }
+
+        #endregion
+
+        #region Methods
+        public List<FeedEntry> GetFeedEntriesByFeed()
+        {
+            return new List<FeedEntry>();
+        }
+        #endregion
+
+
+
     }
 }
