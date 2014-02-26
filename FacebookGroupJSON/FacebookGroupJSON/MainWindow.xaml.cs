e2bde9693ad7a1663e5b6ffcc42ab8c138313867
Daniel Gottschalck <znowm4n@gmail.com>
2014-02-26 11:13:41 +0100

    added other url

--- FacebookGroupJSON/FacebookGroupJSON/MainWindow.xaml.cs
+++ FacebookGroupJSON/FacebookGroupJSON/MainWindow.xaml.cs
@@ -55,7 +55,8 @@ namespace FacebookGroupJSON
             var newW = new AddFeed();
             newW.Show();
 
-            string url = "https://graph.facebook.com/237173582992285/feed?access_token=CAACEdEose0cBACvXQ9zwIad9Ut5qZCiCRV8ClJxOXrZCtZCIUxytkZCt5SzFyYXQ5XLoZB1krQ0HZAjVwZB183DCg9eY1jNx3hGxC0XgObtrh38BAd0QLTkyOpiueZAUERTVmWuwNmXkekVxXQ5zedKMXLT63mIgDnQFeQCDCDIKWVkKsW8ZAnBwNeqZBazUg6xH4ZD";
+            //string url = "https://graph.facebook.com/237173582992285/feed?access_token=CAACEdEose0cBACvXQ9zwIad9Ut5qZCiCRV8ClJxOXrZCtZCIUxytkZCt5SzFyYXQ5XLoZB1krQ0HZAjVwZB183DCg9eY1jNx3hGxC0XgObtrh38BAd0QLTkyOpiueZAUERTVmWuwNmXkekVxXQ5zedKMXLT63mIgDnQFeQCDCDIKWVkKsW8ZAnBwNeqZBazUg6xH4ZD";
+            string url = "https://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=barack%20obama";
 
             Rootobject ro = FeedParser.ParseJsonFromURL(url);
 
